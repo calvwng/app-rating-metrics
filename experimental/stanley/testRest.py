726126b77744abb498fa35a8e676ed9068a02880
Stanley TAng <yelnats916@yahoo.com>
2015-11-12 00:34:02 -0800

    Added negation to sqllite.py sentiment analysis function

--- experimental/stanley/testRest.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-r = requests.get('https://api.appfigures.com/v2/products/search/snapchat?client_key=cadaa98f3bab458a9a321a1505b1e66d', auth=('hello.stanley@yahoo.com', 'stanley123'))
-
-r.status_code
-
-print(r.text)
