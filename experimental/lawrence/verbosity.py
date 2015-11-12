4a348495b994ec82689b3f4932e9a54b1255ab57
LawrZhu <lawrencexzhu@gmail.com>
2015-11-11 20:29:45 -0800

    added verbosity.py

--- experimental/lawrence/verbosity.py
+++ experimental/lawrence/verbosity.py
@@ -7,19 +7,18 @@ histogramlst = {}
 
 
 for i in range(len(reviewlst)):
-           
-           for j in reviewlst[i]:
-                         
-                         #assigns only an array of words with special characters
-                               temp = len(re.split(r'[^0-9A-Za-z]+',j))
+   for j in reviewlst[i]:
+   #assigns only an array of words with special characters
+      temp = len(re.split(r'[^0-9A-Za-z]+',j))
                                      
-                                           if temp not in histogramlst:
-                                                            histogramlst[temp] = 1
-                                                                  else:
-                                                                                   histogramlst[temp] += 1
+      if temp not in histogramlst:
+         histogramlst[temp] = 1
+      else:
+         histogramlst[temp] += 1
 
-                                                                                   print("Number of words : occurance")
-                                                                                   for key in histogramlst:
-                                                                                              print str(key) + " : " + str(histogramlst[key])
+               print("Number of words : occurance")
+
+for key in histogramlst:
+   print str(key) + " : " + str(histogramlst[key])
                                                                                                  
 
