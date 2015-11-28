a6b6c420a1debbd553568d682d12c5ce64280c61
Stanley Tang <stanleytang@Stanleys-MBP.netgear.com>
2015-11-27 22:49:39 -0800

    Added spell check score and updated sentiment analysis

--- experimental/stanley/sqllite.py
+++ experimental/stanley/sqllite.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import enchant
 
 __author__ = 'Ian'
 
@@ -17,7 +18,7 @@ def is_negation(possible_negations, negation_list):
         return -1
     return 1
 
-def sentiment_score(s, negation_words, conn_cursor):
+def sentiment_score(s, negation_words, sent_dict):
     score = 0
     count = 0
     possible_negations = []
@@ -27,21 +28,45 @@ def sentiment_score(s, negation_words, conn_cursor):
         if count > 3:
            possible_negations.pop(0)
         multiplier = 1
-        temp_score = 0
-        for word_score in conn_cursor.execute("SELECT weight FROM KEYWORD WHERE word='{0}'".format(word.encode('utf-8').lower().replace("'", "''"))):
-            temp_score = word_score[0]
-            print "this is the word: " + word
-        if temp_score != 0:
+        if word in sent_dict:
+            temp_score = sent_dict[word]
             score += temp_score * is_negation(possible_negations, negation_words)
+    if score > 5:
+        score = 5
+    if score < -5:
+        score = -5
     return score
 
+
+def spelling_score(s, dict):
+    correct = 0
+    incorrect = 0
+    for word in s.split():
+        if dict.check(word):
+            correct += 1
+        else:
+            incorrect += 1
+    if incorrect == 0:
+        return 5
+    else:
+        incorr_percent = incorrect / float(correct + incorrect)
+        if incorr_percent > 0.3:
+            return 0
+        elif incorr_percent > 0.2:
+            return 1 
+        elif incorr_percent > 0.1:
+            return 2
+        elif incorr_percent > 0.05:
+            return 3
+        else:
+            return 4
+
+dict = enchant.Dict()
 conn = sqlite3.connect(':memory:')
 c = conn.cursor()
 
 creation_sql = ['''CREATE TABLE IF NOT EXISTS APP (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, developer text,
-        release_date DATE, added_date DATE, updated_date DATE, version text)''',
-  ''' CREATE TABLE IF NOT EXISTS KEYWORD (id INTEGER PRIMARY KEY AUTOINCREMENT, word text, weight DECIMAL) '''
-
+        release_date DATE, added_date DATE, updated_date DATE, version text)'''
 ]
 # Create table
 
@@ -57,35 +82,30 @@ c.execute('''CREATE TABLE IF NOT EXISTS REVIEWXKEYWORD
           FOREIGN KEY(REVIEW_ID) REFERENCES REVIEW(ID),
           FOREIGN KEY(KEYWORD_ID) REFERENCES KEYWORD(ID))''')
 
-
-def get_column_names (cursor, table_name):
-    c2 = cursor.execute("select * from " + table_name)
-    return [d[0] for d in c2.description]
-
-print "Adding words:"
-
 conn.commit()
 
+# Populating sentiment analysis dictionary from file
+
+sent_dict = {}
 with open('vader_sentiment_lexicon.txt') as word_file:
     for line in word_file:
         word_weight = line.split()
         if is_number(word_weight[1]):
-            c.execute("INSERT INTO KEYWORD(word, weight) VALUES (?,?)", 
-                     (word_weight[0].decode('utf8'), float(word_weight[1])))
+            sent_dict[word_weight[0]] = float(word_weight[1])
 
 print "Adding reviews:"
-columns = get_column_names(c, "review")
 with open ('snapchatReviews.json') as word_file:
     for line in json.load(word_file):
-        print line['id']
         c.execute("INSERT INTO REVIEW(id, product, original_review, date, author, stars, version) VALUES (?,?,?,?,?,?,?)", (line['id'], line['product'], line['original_review'], line['date'], line['author'], line['stars'], line['version']))
 conn.commit()
 
-# gets a review and assigns score based on KEYWORD table
+# gets reviews and assigns scores
 negation_words = open('negationWords.txt').read().splitlines()
-for row in c.execute("SELECT original_review FROM REVIEW ORDER BY RANDOM() LIMIT 1"):
+for row in c.execute("SELECT original_review FROM REVIEW"):
+    print
     print "this is a review: " + row[0]
-    print "this is the review's score: " + str(sentiment_score(row[0], negation_words, c))
+    print "this is the sentiment score from range -5 to 5: " + str(sentiment_score(row[0], negation_words, sent_dict))
+    print "this is the spelling score from range 0 to 5: " + str(spelling_score(row[0], dict))
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
